0400b18f8d0ad863a135d5cf4e42da3f007c7c25
tsubasa <your_email@examples.com>
2024-01-06 15:07:17 +0900

    Add: bookmark_a-jax, comment_a-jax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,8 @@ class CommentsController < ApplicationController
   before_action :require_login, only: [:create, :edit, :update, :destroy]
 
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to post_path(comment.post), success: 'commentしました'
-    else
-      redirect_to post_path(comment.post), danger: 'comment faild'
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
   end
 
   def update
