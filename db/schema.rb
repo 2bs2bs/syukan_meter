cfaa2da0449fcd478f1c6acb854ab28d9e08596f
tsubasa <your_email@examples.com>
2024-01-08 10:29:00 +0900

    Update: Gemfiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_03_060658) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_bookmarks_on_post_id"
@@ -22,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_060658) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -32,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_060658) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["body"], name: "index_posts_on_body"
@@ -44,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_060658) do
     t.string "avatar"
     t.string "introduction"
     t.string "social_link"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
