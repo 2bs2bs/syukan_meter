f5dc4ed8c32d5535fe259a6c6f1e35594d620209
tsubasa <your_email@examples.com>
2024-01-20 23:33:33 +0900

    Add: CSS in somepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_message_classes(message_type)
+    case message_type
+    when 'success'
+      'bg-green-500 text-black'
+    when 'alert'
+      'bg-red-500 text-black'
+    else
+      'bg-blue-500 text-black'
+    end
+  end
 end
