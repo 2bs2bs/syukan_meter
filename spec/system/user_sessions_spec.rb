bf9c3cf35076618d3a0f1ea3e942517cda8fcb23
tsubasa <your_email@examples.com>
2024-02-09 19:00:11 +0900

    Add: module login_test logout_test in systemspec

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,16 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  # describe 'ログイン前' do
-  #   context '入力が適切' do
-  #     it 'ログイン成功' do
-  #       visit login_path
-  #       fill_in 'Email'
-  #       fill_in 'password'
-  #       click_button 'ログイン'
-  #       expect(page).to have_content 'ログインしました！良い習慣を！'
-  #       expect(current_path).to eq home_path
-  #     end
-  #   end
-  # end
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+
+    context '入力が適切' do
+      it 'ログイン成功' do
+        visit login_path
+        fill_in 'email', with: user.email
+        fill_in 'password', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログインしました！良い習慣を！'
+        expect(current_path).to eq home_path
+      end
+    end
+    
+    context 'メールアドレスがnil' do
+      it 'ログイン失敗' do
+        visit login_path
+        fill_in 'email', with: ''
+        fill_in 'password', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログイン失敗しました'
+        expect(current_path).to eq login_path
+      end
+    end
+
+    context 'パスワードがnil' do
+      it 'ログイン失敗' do
+        visit login_path
+        fill_in 'email', with: user.email
+        fill_in 'password', with: ''
+        click_button 'ログイン'
+        expect(page).to have_content 'ログイン失敗しました'
+        expect(current_path).to eq login_path
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    context 'ログアウト' do
+      it 'ログアウト成功' do
+        login(user)
+
+        # フラッシュメッセージを確認しないとテスト失敗するため注意
+        expect(page).to have_content 'ログインしました！良い習慣を！'
+        
+        visit home_path
+        click_on 'ログアウト'
+        expect(page).to have_content 'ログアウトしました'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
 end
