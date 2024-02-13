d84a194aa65e625125822458b37aef1974eee8c4
tsubasa <your_email@examples.com>
2024-02-13 20:28:07 +0900

    Add: /habit_spec

---
+++ spec/system/habits_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Habits", type: :system do
+  describe 'ログイン前' do
+    describe 'habit create' do
+    end
+  end
+  
+  describe 'ログイン後' do
+    describe 'habit create' do
+    end
+
+    describe 'habit edit' do
+    end
+
+    describe 'habit delstroy' do
+    end
+  end
+end
