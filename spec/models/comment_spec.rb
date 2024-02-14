40b558b184bdd6a780e31fc74781a4abbd7737eb
tsubasa <your_email@examples.com>
2024-02-15 00:19:26 +0900

    Improve: model_spec comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,24 +3,25 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe 'validation check' do
     let(:comment) { build(:comment) }
+
     context 'validation success' do
-      it 'validation thrue' do
+      it 'passes all validations' do
         expect(comment).to be_valid
         expect(comment.errors).to be_empty
       end
     end
 
     context 'validation failed' do
-      it 'body is nil' do
+      it 'fails validation when body is nil' do
         comment.body = nil
         expect(comment).to be_invalid
-        expect(comment.errors[:body]).to eq ["を入力してください"]
+        expect(comment.errors[:body]).to include("を入力してください")
       end
 
-      it 'body over 65_535' do
-        comment.body = 'a' * 65_536
+      it 'fails validation when body length is over 65,535 characters' do
+        comment.body = 'a' * (Comment.validators_on(:body).find { |v| v.options[:maximum] }.options[:maximum] + 1)
         expect(comment).to be_invalid
-        expect(comment.errors[:body]).to eq ["は65535文字以内で入力してください"]
+        expect(comment.errors[:body]).to include("は65535文字以内で入力してください")
       end
     end
   end
