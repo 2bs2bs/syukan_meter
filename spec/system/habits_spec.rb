49fbf1c2a8557b59be84f15b87cd34fdcdd697ac
tsubasa <your_email@examples.com>
2024-02-16 23:38:47 +0900

    Add: thrue test habit create

--- spec/system/habits_spec.rb
+++ spec/system/habits_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Habits", type: :system do
   let(:user) { create(:user) }
-  let(:habit) { create(:habit) }
+  let!(:habit) { create(:habit) }
 
   describe 'ログイン前' do
     context '習慣作成しようとする' do
@@ -15,12 +15,9 @@ RSpec.describe "Habits", type: :system do
   end
   
   describe 'ログイン後' do
-    before do
-      login(user)
-    end
+    before { login(user) }
 
     describe 'habit create' do
-      
       context '入力が適切' do
         it '習慣作成成功' do
           visit habits_path
@@ -28,8 +25,8 @@ RSpec.describe "Habits", type: :system do
           expect(page).to have_selector('#new-habit-modal:not(.hidden)')
           fill_in 'habit_name', with: 'test_habit'
           fill_in 'habit_description', with: 'test_description'
-          fill_in 'start_date', Date.today
-          fill_in 'end_date', habit.end_date
+          fill_in 'habit_start_date', with: Date.today
+          fill_in 'habit_end_date', with: (Date.today + 30)
           click_button '登録する'
           expect(page).to have_content '習慣を作成しました！一緒にがんばりましょう！'
           expect(current_path).to eq habits_path
@@ -43,8 +40,8 @@ RSpec.describe "Habits", type: :system do
           expect(page).to have_selector('#new-habit-modal:not(.hidden)')
           fill_in 'habit_name', with: ''
           fill_in 'habit_description', with: habit.description
-          fill_in 'start_date', with: habit.start_date
-          fill_in 'end_date', with: habit.end_date
+          fill_in 'habit_start_date', with: habit.start_date
+          fill_in 'habit_end_date', with: habit.end_date
           click_button '登録する'
           expect(page).to have_content '習慣作成に失敗しました'
         end
@@ -57,8 +54,8 @@ RSpec.describe "Habits", type: :system do
           expect(page).to have_selector('#new-habit-modal:not(.hidden)')
           fill_in 'habit_name', with: 'a' * 256
           fill_in 'habit_description', with: habit.description
-          fill_in 'start_date', with: habit.start_date
-          fill_in 'end_date', with: habit.end_date
+          fill_in 'habit_start_date', with: habit.start_date
+          fill_in 'habit_end_date', with: habit.end_date
           click_button '登録する'
           expect(page).to have_content '習慣作成に失敗しました'
         end
@@ -71,8 +68,8 @@ RSpec.describe "Habits", type: :system do
           expect(page).to have_selector('#new-habit-modal:not(.hidden)')
           fill_in 'habit_name', with: ''
           fill_in 'habit_description', with: 'a' * 1001
-          fill_in 'start_date', with: habit.start_date
-          fill_in 'end_date', with: habit.end_date
+          fill_in 'habit_start_date', with: habit.start_date
+          fill_in 'habit_end_date', with: habit.end_date
           click_button '登録する'
           expect(page).to have_content '習慣作成に失敗しました'
         end
@@ -85,20 +82,53 @@ RSpec.describe "Habits", type: :system do
           expect(page).to have_selector('#new-habit-modal:not(.hidden)')
           fill_in 'habit_name', with: ''
           fill_in 'habit_description', with: habit.description
-          fill_in 'start_date', with: ''
-          fill_in 'end_date', with: habit.end_date
+          fill_in 'habit_start_date', with: ''
+          fill_in 'habit_end_date', with: habit.end_date
           click_button '登録する'
           expect(page).to have_content '習慣作成に失敗しました'
         end
       end
 
       context 'end_date is nil' do
+        it 'create failed' do
+          visit habits_path
+          click_on '習慣を作成する'
+          expect(page).to have_selector('#new-habit-modal:not(.hidden)')
+          fill_in 'habit_name', with: habit.name
+          fill_in 'habit_description', with: habit.description
+          fill_in 'habit_start_date', with: habit.start_date
+          fill_in 'habit_end_date', with: ''
+          click_button '登録する'
+          expect(page).to have_content '習慣作成に失敗しました'
+        end
       end
 
       context 'start_date < Today' do
+        it 'create failed' do
+          visit habits_path
+          click_on '習慣を作成する'
+          expect(page).to have_selector('#new-habit-modal:not(.hidden)')
+          fill_in 'habit_name', with: habit.name
+          fill_in 'habit_description', with: habit.description
+          fill_in 'habit_start_date', with: (habit.start_date - 1)
+          fill_in 'habit_end_date', with: habit.end_date
+          click_button '登録する'
+          expect(page).to have_content '習慣作成に失敗しました'
+        end
       end
 
       context 'start_date > end_date' do
+        it 'create failed' do
+          visit habits_path
+          click_on '習慣を作成する'
+          expect(page).to have_selector('#new-habit-modal:not(.hidden)')
+          fill_in 'habit_name', with: habit.name
+          fill_in 'habit_description', with: habit.description
+          fill_in 'habit_start_date', with: habit.end_date
+          fill_in 'habit_end_date', with: habit.start_date
+          click_button '登録する'
+          expect(page).to have_content '習慣作成に失敗しました'
+        end
       end
     end
 
