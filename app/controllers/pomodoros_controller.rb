d92b9ada8797d2241c4e0ea1d683fec8c1f6af1f
tsubasa <your_email@examples.com>
2024-03-04 17:31:24 +0900

    Update: Timer update

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -2,4 +2,20 @@ class PomodorosController < ApplicationController
   def index
     @pomodoro = Pomodoro.new
   end
+
+  def create
+    @pomodoro = Pomodoro.new(pomodoro_params)
+    
+    if @pomodoro.save
+      render json: @pomodoro, status: :created
+    else
+      render json: @pomodoro.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pomodoro_params
+    params.require(:pomodoro).permit(:started_at, :ended_at)
+  end
 end
