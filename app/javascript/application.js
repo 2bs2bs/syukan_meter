e770ec8739e31862577530580e42bb4a88ac8cb3
tsubasa <tsubasa19957629@au.com>
2024-04-01 17:47:44 +0900

    Update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "toggle_user_new_form"
 
 // headerのnavber
 document.addEventListener('turbo:load', () => {
   const avatar = document.getElementById('avatar');
   const navbar = document.getElementById('navbar');
-
+  
   avatar.addEventListener('click', () => {
     navbar.classList.toggle('hidden');
   });
 });
+
+import "./toggle_user_new_form"
