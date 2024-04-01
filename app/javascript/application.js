d9eacf7fced8aeaf7e86ec565008d2d6636b08ef
tsubasa <tsubasa19957629@au.com>
2024-04-01 17:42:34 +0900

    Update: toggle_user_new_form_pass

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./toggle_user_new_form"
+import "toggle_user_new_form"
 
 // headerのnavber
 document.addEventListener('turbo:load', () => {
